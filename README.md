c7b01ad247f39830223893251df5aad1e8b61f2b
abhijeetk698 <abhijeetk698@gmail.com>
2020-05-31 00:00:58 +0530

    setup new campground post route

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Each campground has
 * create out header and footer partials
 * Add in Bootstrap
 
+# Creating New Campgrounds
+* Setup new campground POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
+
--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express=require("express");
+const bodyParser=require("body-parser");
 const app=express();
 
 app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+// NAKLI DATABASE
+var element=[
+    {
+        name:"everest base camp",
+        img: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name:"forest gump",
+        img: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name:"friend camp",
+        img: "https://images.unsplash.com/photo-1530541930197-ff16ac917b0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    }
+];
 
 app.get("/",(req,res)=>{
     res.redirect("/landing");
@@ -12,21 +31,21 @@ app.get("/landing",(req,res)=>{
 });
 
 app.get("/campgrounds",(req,res)=>{
-    var element=[
-        {
-            name:"everest base camp",
-            img: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-        },
-        {
-            name:"forest gump",
-            img: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-        },
-        {
-            name:"friend camp",
-            img: "https://images.unsplash.com/photo-1530541930197-ff16ac917b0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-        }
-    ];
+    
     res.render("campgrounds",{element:element});
+});
+
+app.get("/campgrounds/new",(req,res)=>{
+    res.render("new");
+})
+
+app.post("/campgrounds",(req,res)=>{
+    var camp={
+        name:req.body.name,
+        img:req.body.img
+    }
+    element.push(camp);
+    res.redirect("/campgrounds");
 });
 
 app.listen(2020,()=>{
